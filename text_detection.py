612b2e0db1ade8ba7e3d1dc5de5ca848f902a0c8
henahkoo <30427183+henahkoo@users.noreply.github.com>
2019-03-25 18:47:40 +0900

    Update text_detection.py

--- text_detection.py
+++ text_detection.py
@@ -15,9 +15,9 @@ ap.add_argument("-east", "--east", type=str,
 	help="path to input EAST text detector")
 ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
 	help="minimum probability required to inspect a region")
-ap.add_argument("-w", "--width", type=int, default=320,
+ap.add_argument("-w", "--width", type=int, default=224,
 	help="resized image width (should be multiple of 32)")
-ap.add_argument("-e", "--height", type=int, default=320,
+ap.add_argument("-e", "--height", type=int, default=224,
 	help="resized image height (should be multiple of 32)")
 args = vars(ap.parse_args())
 
